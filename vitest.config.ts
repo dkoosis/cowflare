c8ce99269be8f98bca487a995d2d3ee2fd45ba94
dkoosis <dkoosis@gmail.com>
2025-07-18 22:47:18 -0400

    testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,11 @@ export default defineWorkersConfig({
 	test: {
 		poolOptions: {
 			workers: {
-				wrangler: { configPath: "./wrangler.test.jsonc" },
+				wrangler: { configPath: "./wrangler.jsonc" },
+				miniflare: {
+					compatibilityFlags: ["nodejs_compat", "export_commonjs_default"],
+					compatibilityDate: "2022-10-31",
+				},
 			},
 		},
 		globals: true,
